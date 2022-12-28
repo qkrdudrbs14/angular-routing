41cd845cf0ebc95350a1237b146c7271bd5e0bd4
Young Gyun Park <qkrdudrbs14@gmail.com>
2022-12-28 21:48:44 +0900

    simple router root

--- README.md
+++ README.md
@@ -26,7 +26,17 @@ Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To u
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
 
-##############################################################################################################################################
-router 예제
+### router 예제
+
+https://angular.io/guide/router
+따라하기
 
 ng generate component heroes
+
+ng g c first
+ng g c second
+
+routerLink="/first-component"
+routerLinkActive="active"
+ariaCurrentWhenActive="page"
+<router-outlet></router-outlet>
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    ProductDetailsComponent,
+    FirstComponent,
+    SecondComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
